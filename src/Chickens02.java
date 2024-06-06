4e5c5e7c47673411bfb4f44b5f31ea7f58a35989
Aluno <Aluno@10.85.198.41>
2024-06-06 14:08:46 -0300

    Commit project Chickens02

---
+++ src/Chickens02.java
@@ -0,0 +1,14 @@
+public class Chickens02 {
+    public static void main(String[] args) {
+        //Put yout code here
+        int mediadiaria, mediamensal, lucromensal;
+        mediadiaria = (int) 122.66666666667; // media diaria de ovos podendo ser arrecadada pra lucro
+        mediamensal = (int) 3380.0;  // media mensal de ovos
+        lucromensal = (int) 608.8;  // lucro mensal de ovos
+
+        System.out.println("Media diaria: " +mediadiaria);  //imprime a media diaria de ovos
+        System.out.println("Media mesal: " +mediamensal);  // imprime a media mensal de ovos
+        System.out.println("lucro mensal:  $" +lucromensal); //imprime o lucro mensal de ovos
+    }
+    
+}
